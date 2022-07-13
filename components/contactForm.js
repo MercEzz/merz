cbc7eea339dd74edda93cb2a9feb2b701f10ec95
mitesh virash <miteshkvirash@gmail.com>
2022-07-13 18:38:46 +0530

    contactpg

--- components/contactForm.js
+++ components/contactForm.js
@@ -7,8 +7,25 @@ import {
   useColorModeValue,
   Flex,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    msg: "",
+  });
+
+  const { name, email, msg } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <Flex
       p="5"
@@ -18,17 +35,37 @@ const ContactForm = () => {
       m="2"
     >
       <FormControl>
-        <Stack direction={"column"} spacing="5">
-          <Input variant="flushed" id="name" placeholder="Name" />
-          <Input
-            variant="flushed"
-            id="email"
-            type="email"
-            placeholder="Email"
-          />
-          <Textarea variant="flushed" placeholder="Your Message" />
-          <Button variant="ghost">Submit</Button>
-        </Stack>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <Stack direction={"column"} spacing="5">
+            <Input
+              variant="flushed"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => onChange(e)}
+              placeholder="Name"
+            />
+            <Input
+              variant="flushed"
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
+            <Textarea
+              variant="flushed"
+              placeholder="Your Message"
+              name="msg"
+              value={msg}
+              onChange={(e) => onChange(e)}
+            />
+            <Button type="submit" variant="ghost">
+              Submit
+            </Button>
+          </Stack>
+        </form>
       </FormControl>
     </Flex>
   );
