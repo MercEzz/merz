917e303bd43be2d01be60225cd3f61aed03cce0a
mitesh virash <miteshkvirash@gmail.com>
2022-09-20 17:58:46 +0530

    updated

--- components/homePage.js
+++
@@ -1,9 +0,0 @@
-import { Container, Box, useColorModeValue, Flex } from "@chakra-ui/react";
-
-import MainTitle from "./MainTitle";
-
-const HomePage = () => {
-  return <></>;
-};
-
-export default HomePage;
--- pages/_app.js
+++ pages/_app.js
@@ -6,17 +6,20 @@ import theme from "../theme/theme";
 import "@fontsource/poppins/600.css";
 import "@fontsource/quicksand/300.css";
 import Head from "next/head";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <Head>
         <link rel="icon" href="/m.png" />
       </Head>
       <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <motion.div key={router.route}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </motion.div>
       </ChakraProvider>
     </>
   );
